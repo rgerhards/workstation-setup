f3f8ab2033b340984154670ec4957bfc3e15d398
Rainer Gerhards <rgerhards@adiscon.com>
2019-07-22 16:27:39 +0200

    add Docker setup

--- ubuntu/18.04/setup-system.sh
+++ ubuntu/18.04/setup-system.sh
@@ -61,6 +61,27 @@ sudo	apt-get install -y \
 	wget \
 	zlib1g-dev
 
+# install Docker according to their official guide
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+sudo systemctl start docker
+#exit ## Testing aid - uncomment to skip building software! ##
+
+
 mkdir ~/proj
 cd ~/proj
 rm -rf codestyle libksi librdkafka
